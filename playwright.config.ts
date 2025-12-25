209b462c83bb397def6d5999e2ab644ad5bea3fc
Paramita0081 <paramitamaity827@gmail.com>
2025-12-25 21:29:00 +0530

    changes in yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,24 @@
-import { defineConfig, devices } from '@playwright/test';
-import { defineBddConfig } from "playwright-bdd";
-/** 
+import { defineConfig, devices } from "@playwright/test";
+import { cucumberReporter, defineBddConfig } from "playwright-bdd";
+/**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
- import dotenv from 'dotenv';
- import path from 'path';
- dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 
 const testDir = defineBddConfig({
-    features: 'tests/features/**/*.feature',
-    steps :["tests/step-definitions/*.ts", "tests/fixtures/*.ts"],
-
+  features: "tests/features/**/*.feature",
+  steps: ["tests/step-definitions/*.ts", "tests/fixtures/*.ts"],
 });
 
 export default defineConfig({
-    testDir,
+  testDir,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -29,10 +28,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-   globalSetup: require.resolve('./tests/helpers/global-setup.ts'),
+  globalSetup: require.resolve("./tests/helpers/global-setup.ts"),
   reporter: [
-    ['html']
-   // ["allure-playwright"]
+    ["html"],
+    //  ['allure-playwright'],
+    // ['list'],
+    cucumberReporter("html", { outputFile: "cucumber-report/index.html" }),
+    cucumberReporter("json", { outputFile: "cucumber-report/results.json" }),
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -40,27 +42,30 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    ignoreHTTPSErrors:true,
-  //  navigationTimeout:30_000
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "retain-on-failure",
+    timezoneId: "UTC",
+    ignoreHTTPSErrors: true,
+    //  navigationTimeout:30_000
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //  name: 'firefox',
+    //  use: { ...devices['Desktop Firefox'] },
+    //},
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/fixtures/fixture.ts
+++ tests/fixtures/fixture.ts
@@ -1,16 +1,30 @@
-import { test as base} from "playwright-bdd";
-import * as Pages from  "../pageobjects/index";
+import { test as base, createBdd } from "playwright-bdd";
+import * as Pages from "../pageobjects/index";
+import { Page } from "playwright-core";
 
 type PageFixtures = {
-   multiWindowPage: Pages.MultiWindowPage;
-}
+  multiWindowPage: Pages.MultiWindowPage;
+};
+
+const createTestFunction =
+  <T extends new (page: Page) => InstanceType<T>>(PageClass: T) =>
+  (
+    { page }: { page: Page },
+    use: (fixture: InstanceType<T>) => Promise<void>
+  ) =>
+    use(new PageClass(page));
 
 export const test = base.extend<PageFixtures>({
+  multiWindowPage: createTestFunction(Pages.MultiWindowPage),
+});
+
+/*export const test = base.extend<PageFixtures>({
  multiWindowPage: async ({ page }, use) => {
    const multiWindowPageObj = new Pages.MultiWindowPage(page);
    await use(multiWindowPageObj);
    }
 });
+*/
 
-//export const { Given, When, Then, After } = createBdd(test);
-//export { expect } from "@playwright/test";
+
+export const bdd = createBdd(test);
